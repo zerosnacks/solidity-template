5a75f623d0413d44de74086c9f8eb76d5171470d
zerosnacks <zerosnacks@protonmail.com>
2022-06-09 13:50:12 +0200

    update flatten script

--- scripts/flatten.sh
+++ scripts/flatten.sh
@@ -5,7 +5,8 @@ echo Which contract do you want to flatten \(e.g. Greeter\)?
 read contract
 
 # Clean up temporary file
-rm -rf flattened.sol
+mkdir -p out
+rm -rf out/flattened.sol
 
 # Flatten to temporary file
-forge flatten ./src/${contract}.sol > flattened.sol
+forge flatten ./src/${contract}.sol > ./out/flattened.sol
